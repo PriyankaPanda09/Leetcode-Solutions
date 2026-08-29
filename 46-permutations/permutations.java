f369eb54480bea90254d9749d1144dd9c12a942f
Priyanka Panda <158509146+PriyankaPanda09@users.noreply.github.com>
2026-08-30 00:40:18 +0530

    Time: 1 ms (87.95%) | Memory: 45.9 MB (8.54%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,31 @@
 class Solution {
 
-    private void recurePermute(int[] nums, List<Integer> ds,List<List<Integer>> ans,boolean []freq){
-        if(ds.size()== nums.length){
+    private void recurePermute(int index, int[] nums, List<List<Integer>> ans) {
+        if (index == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.add(nums[i]);
-                recurePermute(nums,ds,ans,freq);
-                ds.remove(ds.size()-1);
-                freq[i] = false;
-            }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurePermute(index + 1, nums, ans);
+            swap(i, index, nums);
         }
+    
+    }
+
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        recurePermute(nums,ds,ans,freq);
+        recurePermute(0, nums, ans);
         return ans;
-        
     }
 }
