f2c26761b2051d8356bf8a18152ce8b257aba597
Priyanka Panda <158509146+PriyankaPanda09@users.noreply.github.com>
2026-08-31 14:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,24 +1,35 @@
 // using dynamic programming
 class Solution {
-    public int f(int n,List<Integer> dp){
+    // public int f(int n,List<Integer> dp){
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     if(dp.get(n) != -1){
+    //         return dp.get(n);
+    //     }
+    //     int ans=  f(n-1,dp) + f(n-2,dp);
+    //     dp.set(n,ans);
+
+    //     return ans;
+    // }
+    public int fib(int n) {
+        // List<Integer> dp = new ArrayList<>();
+        
+        // // initialize dp with -1
+        // for(int i=0;i<=n;i++){
+        //     dp.add(-1);
+        // }
+        // return f(n,dp);
+        int prev2= 0;
+        int prev1 =1;
         if(n<=1){
             return n;
         }
-        if(dp.get(n) != -1){
-            return dp.get(n);
+        for(int i=2;i<=n;i++){
+            int curi = prev1 +prev2;
+            prev2 = prev1;
+            prev1= curi;
         }
-        int ans=  f(n-1,dp) + f(n-2,dp);
-        dp.set(n,ans);
-
-        return ans;
-    }
-    public int fib(int n) {
-        List<Integer> dp = new ArrayList<>();
-        
-        // initialize dp with -1
-        for(int i=0;i<=n;i++){
-            dp.add(-1);
-        }
-        return f(n,dp);
+        return prev1;
     }
 }
