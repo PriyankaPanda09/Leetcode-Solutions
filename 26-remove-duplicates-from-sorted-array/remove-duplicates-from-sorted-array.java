8bbbf02596e00193f5bb9a4decdba8380999e0b5
Priyanka Panda <158509146+PriyankaPanda09@users.noreply.github.com>
2026-09-14 16:27:24 +0530

    Time: 6 ms (3.46%) | Memory: 46.8 MB (42.76%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         Set<Integer> expectedNums = new TreeSet<>();
+        // here the question demands the result to be sorted in scending order only
+        // but hashSet doesnot gurantee soreted order so we use treeSet which gurantee sorted order.
         for(int i=0;i<nums.length;i++){
             expectedNums.add(nums[i]);
         }
@@ -11,4 +13,7 @@ class Solution {
         }
         return k;
     }
+    // tc : NlogN + N;
+    // sc : N
+    // we can minimize the time complexity by o(N) and sc by o(1) using 2 pointers
 }
